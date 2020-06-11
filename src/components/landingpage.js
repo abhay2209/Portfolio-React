9725af8b96d00f3bf166e2166d3cfa6ee67fc3f6
Abhay Jolly <abhayjolly18@gmail.com>
2020-06-11 02:44:28 -0700

    Added profile picture and description on main page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,25 @@
 import React, {Component } from 'react';
-
+import {Grid, Cell} from 'react-mdl'
 class Landing extends Component {
     render() {
         return(
-            <div><h1>Landing page</h1></div>
+            <div style ={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                    <img 
+                    src = {require("../AbhayJolly.jpg")}
+                
+                    alt="avatar"
+                    className="avatar-img"/>
+                    
+                    <div className="banner-text">
+                            <h1>Abhay Jolly</h1>
+                            <hr/>
+                            <p> C++ | Python | C | JavaScript | NodeJS | React | SQL | Express | HTML/CSS </p>
+                    </div>
+                    </Cell>
+                </Grid>
+                </div>
         )
     }
 }
