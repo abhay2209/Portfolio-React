f472b6cc3ef110c25bea6e1d6ab0e07c6d50f02f
Abhay Jolly <abhayjolly18@gmail.com>
2020-08-24 18:05:52 -0700

    Home page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -16,6 +16,15 @@ class Landing extends Component {
                             <h1>Abhay Jolly</h1>
                             <hr/>
                             <p> C++ | Python | C | JavaScript | NodeJS | React | SQL | Express | HTML/CSS </p>
+                            <div class ="social-links">
+                                <a href ="https://www.linkedin.com/in/abhay-jolly-585b49196/" target="_blank" rel="noopener noreferrer">
+                                    <i class ="fa fa-linkedin-square"  aria-hidden="true"></i>
+                                </a>
+                                <a href ="https://github.com/abhay2209" target="_blank" rel="noopener noreferrer">
+                                    <i class ="fa fa-github-square"  aria-hidden="true"></i>
+                                </a>
+
+                            </div>
                     </div>
                     </Cell>
                 </Grid>
